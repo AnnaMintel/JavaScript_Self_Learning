21971f74112b0be1230fdd52a557f8eb4dc0e1b3
Anna <annikamintel99@yandex.ru>
2021-11-28 22:49:47 +0300

    feat: have learnt array methods

--- Lessons/script.js
+++ Lessons/script.js
@@ -1,4 +1,4 @@
-/* ЗАДАЧА 1. Если посетителю 12 лет или больше,
+/*              ЗАДАЧА 1. Если посетителю 12 лет или больше,
 он может проходить. Если ему еще не исполнилось 12, но его
 сопровождает взрослый, пусть тоже проходит. Во всех
 остальных случаях вход запрещен.
@@ -7,9 +7,35 @@ let age = 13;
 let accessableAge = 12;
 let canEnter = age >= accessableAge;
 let withParents = false;
+console.log(canEnter || withParents); 
 
-console.log(canEnter || withParents); */
 
-/* ЗАДАЧА 2.
+                ЗАДАЧА 2. Добавление элементов в пустой массив.
 
- */
+let animals = [];
+animals.push("Cat");
+animals.push("Dog");
+animals.unshift("Monkey");
+console.log(animals);   
+
+                ЗАДАЧА 3. Перезаписать последний элемент массива в начало
+let a = [1, 2, 3, 4];
+let lastNum = a.pop();
+a.unshift(lastNum);
+console.log(a); 
+
+    ВАРИАНТ 2. Перезапишем все элементы массива через цикл
+let a = [1, 2, 3, 4];
+for (let i = 0; i < a.length; i++) {
+    let lastNum = a.pop();
+    a.unshift(lastNum);
+    console.log(a);
+}
+
+*/
+
+
+
+
+
+

27d0c671ce40ec5038b9a7d92d2be746031e24d1
Anna <annikamintel99@yandex.ru>
2021-11-28 19:35:20 +0300

    init: created project with lessons

---
+++ Lessons/script.js
@@ -0,0 +1,15 @@
+/* ЗАДАЧА 1. Если посетителю 12 лет или больше,
+он может проходить. Если ему еще не исполнилось 12, но его
+сопровождает взрослый, пусть тоже проходит. Во всех
+остальных случаях вход запрещен.
+
+let age = 13;
+let accessableAge = 12;
+let canEnter = age >= accessableAge;
+let withParents = false;
+
+console.log(canEnter || withParents); */
+
+/* ЗАДАЧА 2.
+
+ */

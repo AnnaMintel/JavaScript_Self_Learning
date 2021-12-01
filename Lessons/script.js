b70d916fd8be79f42ea48f8b6e399aa211c811c3
Anna <annikamintel99@yandex.ru>
2021-12-01 22:16:09 +0300

    feat: completed learning arrays

--- Lessons/script.js
+++ Lessons/script.js
@@ -2,7 +2,6 @@
 он может проходить. Если ему еще не исполнилось 12, но его
 сопровождает взрослый, пусть тоже проходит. Во всех
 остальных случаях вход запрещен.
-
 let age = 13;
 let accessableAge = 12;
 let canEnter = age >= accessableAge;
@@ -11,7 +10,6 @@ console.log(canEnter || withParents);
 
 
                 ЗАДАЧА 2. Добавление элементов в пустой массив.
-
 let animals = [];
 animals.push("Cat");
 animals.push("Dog");
@@ -23,7 +21,6 @@ let a = [1, 2, 3, 4];
 let lastNum = a.pop();
 a.unshift(lastNum);
 console.log(a); 
-
     ВАРИАНТ 2. Перезапишем все элементы массива через цикл
 let a = [1, 2, 3, 4];
 for (let i = 0; i < a.length; i++) {
@@ -32,12 +29,26 @@ for (let i = 0; i < a.length; i++) {
     console.log(a);
 }
                 ЗАДАЧА 4. Объединение массивов.
-*/
 let animalsWild = ["Tiger", "Lion"];
 let animalsHome = ["Cat"];
 let animalsInZoo = animalsWild.concat(animalsHome);
-
 console.log(animalsInZoo);   
+
+                ЗАДАЧА 5.Random
+let randomBody = ["глаз", "нос", "череп"];
+let randomAdjectives = ["вонючая", "унылая", "дурацкая"];
+let randomWords = ["муха", "выдра", "дубина", "мартышка", "крыса"];
+// Выбор случайной части тела из массива randomBody:
+let randomBodyParts = randomBody[Math.floor(Math.random() * 3)];
+// Выбор случайного прилагательного из массива randomAdjectives:
+let randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+ // Выбор случайного слова из массива randomWords:
+let randomWord = randomWords[Math.floor(Math.random() * 5)];
+// Соединяем случайные строки в предложение:
+let randomRes = "У тебя " + randomBodyParts + " словно " + 
+randomAdjective + " " + randomWord + "!!!";
+console.log(randomRes);
+*/
 
 
 

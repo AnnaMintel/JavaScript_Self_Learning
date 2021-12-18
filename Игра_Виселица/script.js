b90f86a6f3db51f8b422a16c9d00efe44a4f5a98
Anna <annikamintel99@yandex.ru>
2021-12-18 14:09:26 +0300

    feat: rewrote game using functions

--- Игра_Виселица/script.js
+++ Игра_Виселица/script.js
@@ -9,7 +9,7 @@ _ _ _ _ _ _ _
 _ А _ _ _ _ А
 Если второй игрок назовет букву, которой нет в слове, у него отнимается очко, а первый игрок рисует руку, ногу или другую часть тела
 человечка. Если первый игрок закончит рисовать человечка раньше, чем
-второй угадает все буквы, второй игрок проиграл. */
+второй угадает все буквы, второй игрок проиграл. 
 
 let words = [
     "morning",
@@ -60,6 +60,65 @@ while (remainingLetters > 0 && attempts > 0) {
     // Конец игрового цикла
 }
 
+// Отображаем ответ и поздравляем игрока
+alert(answerArray.join(" "));
+alert("Отлично! Было загадано слово " + word);
+
+ПЕРЕПИСЫВАЕМ ИГРУ ПОД ФУНКЦИИ:
+
+*/
+let words = [
+    "morning",
+    "cinema",
+    "poster",
+    "wife",
+    "sport",
+    "lessons",
+    "wolf"
+];
+
+// Выбираем случайное слово
+let word = words[Math.floor(Math.random() * words.length)];
+
+// Создаем итоговый массив
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+
+let remainingLetters = word.length;
+let attempts = 10;
+
+// Игровой цикл
+function game() {
+while (remainingLetters > 0 && attempts > 0) {
+
+    // Показываем состояние игры
+    alert(answerArray.join(" "));
+
+    // Запрашиваем вариант ответа
+    let value = prompt("Угадайте букву или нажмите Отмена для выхода из игры.").toLowerCase();
+    if (value === null) {
+        // Выходим из игрового цикла
+        break;
+    } else if (value.length !== 1) {
+        alert("Пожалуйста, введите только одну букву.");
+        attempts--;
+    } else {
+        // Обновляем состояние игры
+        for (let j = 0; j < word.length; j++) {
+            if (word[j] === value && answerArray[j] !== word[j]) {
+                answerArray[j] = value;
+                remainingLetters--;
+            };
+        };
+        attempts--;
+    };
+    // Конец игрового цикла
+};
+};
+
+game();
 // Отображаем ответ и поздравляем игрока
 alert(answerArray.join(" "));
 alert("Отлично! Было загадано слово " + word);

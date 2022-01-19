e58073fa77d347089f9add0dd0ceab60850b041f
AnnaMintel <annikamintel99@yandex.ru>
2022-01-19 22:26:20 +0300

    init: created project WOOF WOOF (train functions)

---
+++ HEAD FIRST JavaScript/WOOF WOOF/script.js
@@ -0,0 +1,10 @@
+// WOOF WOOF
+
+let dogNaME = "rover";
+let dogWeight = 20;
+
+if (dogWeight > 20){
+    console.log(dogNaME + " says WOOF WOOF");
+} else {
+    console.log(dogNaME + " says woof woof");
+}

0857a87ab6d4818891fb4550e87eef21ee5413d1
AnnaMintel <annikamintel99@yandex.ru>
2022-02-08 14:48:55 +0300

    feat: practicing jquery

--- HEAD FIRST JavaScript and JS for children/DOM and jQuery/script.js
+++ HEAD FIRST JavaScript and JS for children/DOM and jQuery/script.js
@@ -1,5 +1,10 @@
-let h1 = document.getElementById("main-h1");
+/* let h1 = document.getElementById("main-h1");
 console.log(h1.innerHTML);
 
 let newH1 = prompt("Enter new title: ");
 h1.innerHTML = newH1 ;
+*/
+
+// jquery
+let newH1 = prompt("Enter new title: ");
+$("#main-h1s").text(newH1);

c9780aca9ca35af230852d70da4348343d040900
AnnaMintel <annikamintel99@yandex.ru>
2022-02-09 23:41:09 +0300

    feat: practised THIS

--- HEAD FIRST JavaScript and JS for children/objects/script.js
+++ HEAD FIRST JavaScript and JS for children/objects/script.js
@@ -1,3 +1,4 @@
+/* add method through object
 let dog = {
     name: "Gavv",
     legs: 4
@@ -7,3 +8,19 @@ dog.bark = function() {
     console.log("Bark! My name is " + this.name + "!");
 };
 dog.bark();
+*/
+
+/* using THIS 
+let car = {
+    door: 4,
+    color: 'red',
+    model: function (){
+        if (this.door == 4){
+            console.log('Tesla');
+        } else {
+            console.log('sport');
+        }
+    }
+}
+car.model();
+*/

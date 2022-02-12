4fc081488c9ef6a3513c6017096fd3b9f25825c8
AnnaMintel <annikamintel99@yandex.ru>
2022-02-13 00:24:19 +0300

    feat: trained evenr listener

--- HEAD FIRST JavaScript and JS for children/events handler/script.js
+++ HEAD FIRST JavaScript and JS for children/events handler/script.js
@@ -6,7 +6,8 @@ function pageLoaded () {
 window.onload = pageLoaded;
 */
 
-/* */
+
+/* move callback to common variable
 let callback = (a) => {console.log('i have clicked')};
 
 let block = document.querySelector('.block');
@@ -16,3 +17,19 @@ let block2 = document.querySelector('.block2');
 block.addEventListener('dblclick', callback );
 block1.addEventListener('click', callback );
 block2.addEventListener('mouseover', callback );
+*/
+
+
+/* working with different blocks
+let callback1 = (a) => {console.log('i have clicked on BLUE')};
+let callback2 = (a) => {console.log('i have clicked on PINK')};
+let callback3 = (a) => {console.log('i have clicked on GREEN')};
+
+let block = document.querySelector('.block');
+let block1 = document.querySelector('.block1');
+let block2 = document.querySelector('.block2');
+
+block.addEventListener('click', callback1 );
+block1.addEventListener('click', callback2);
+block2.addEventListener('click', callback3 );
+*/
